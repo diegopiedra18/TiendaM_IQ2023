ce82a046ec275baa09a0492c807fb042df2a9f7b
diepi <diepi@DESKTOP-2V9FICE>
2023-02-08 20:07:14 -0600

    creacion del proyecto inicial

---
+++ src/main/java/com/TiendaM_lQ2023/TiendaMLQ2023Application.java
@@ -0,0 +1,13 @@
+package com.TiendaM_lQ2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMLQ2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaMLQ2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TiendaM_lQ2023/TiendaMLQ2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaM_lQ2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMLQ2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
